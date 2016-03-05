5735655b5054e77da48d82780b147a0c5ce44ae6
Thuong Ho <thuong.t.ho@gmail.com>
2016-03-05 13:34:07 -0800

    add services

---
+++ www/js/services.js
@@ -0,0 +1,17 @@
+angular.module('starter.services', [])
+
+.factory('WireWaxAPI', ['$http', '$q', function ($http, $q) {
+  
+
+  return {
+
+  };
+}])
+
+.factory('TwitterAPI', ['$http', '$q', function ($http, $q) {
+  
+
+  return {
+
+  };
+}])
